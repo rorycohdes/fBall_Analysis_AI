b2690fcbdee090f9e167620f87ecf7de00c80320
rorycohdes <roryallen58@gmail.com>
2024-12-29 09:51:07 -0500

    documentation

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,12 +1,14 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+model = YOLO('yolov8x') 
 
-results = model.predict('input_videos/08fd33_4.mp4', save = True)
+results = model.predict('input_videos/08fd33_4.mp4', save = True) 
 
 print(results[0])
 
 print("=======================")
 
+
+# logs the data from all the bounding boxes created by the model
 for box in results[0].boxes:
-    print(box)
+    print(box) 
